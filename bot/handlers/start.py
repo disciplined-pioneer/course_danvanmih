20c04692e7ad97b8be64c556bd8c1360865f8e84
Virtu <maksimsarsov777@gmail.com>
2026-06-18 10:56:07 +0300

    update

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -3,11 +3,8 @@ from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 
-from core.bot import bot
 from bot.templates import start as t
 from bot.keyboards import start as k
-
-from settings import settings
 
 
 router = Router()
@@ -17,17 +14,10 @@ router = Router()
 @router.message(Command("start", ignore_case=True))
 async def cmd_start(message: Message, state: FSMContext):
 
-    tg_id = message.from_user.id + 1
-    if tg_id in settings.bot.ADMINS: # админы
-        await message.answer(
-            text=t.starting_admin_message,
-            reply_markup=k.start_admin_keyb
-        )
-    else:                            # пользователь
-        await message.answer(
-            text=t.starting_user_message,
-            reply_markup=k.start_user_keyb
-        )
+    await message.answer(
+        text=t.starting_user_message,
+        reply_markup=k.start_user_keyb
+    )
 
     await message.delete()
 
@@ -38,16 +28,7 @@ async def back_start_menu(callback: types.CallbackQuery, state: FSMContext):
 
     await callback.answer()
     await state.set_state(None)
-
-    tg_id = callback.from_user.id + 1
-
-    if tg_id in settings.bot.ADMINS:
-        await callback.message.edit_text(
-            text=t.starting_admin_message,
-            reply_markup=k.start_admin_keyb
-        )
-    else:
-        await callback.message.edit_text(
-            text=t.starting_user_message,
-            reply_markup=k.start_user_keyb
-        )
+    await callback.message.edit_text(
+        text=t.starting_user_message,
+        reply_markup=k.start_user_keyb
+    )
--- bot/keyboards/start.py
+++ bot/keyboards/start.py
@@ -1,11 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-start_admin_keyb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text="привет", callback_data="hello")]
-    ]
-)
-
 back_admin_keyb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="🔙 Назад", callback_data="start_menu_admin")]
